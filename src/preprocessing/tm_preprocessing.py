58885b60e241dff1b0195dac6625b39170efd4de
Poto <enversionkiller@gmail.com>
2023-03-22 12:20:18 +0800

    added word2vec embedding and pca/svd

--- src/preprocessing/tm_preprocessing.py
+++ src/preprocessing/tm_preprocessing.py
@@ -2,18 +2,20 @@ import os
 import numpy as np
 import pandas as pd
 import datetime
+from sklearn.decomposition import TruncatedSVD, PCA
 
 # Other NLP Libraries
 import nltk
 from nltk.stem import WordNetLemmatizer
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
 
 # Gensim
 import gensim
 from gensim import corpora, models
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
+from gensim.models import KeyedVectors
 
 def tm_preprocess(reviewText, min_len=2, max_len=15):
 	# Tokenize and Remove stopwords
@@ -24,29 +26,30 @@ def tm_preprocess(reviewText, min_len=2, max_len=15):
 	return lemmaText
 
 if __name__ == "__main__":
-    
-	positive_text = pd.DataFrame(columns=["Text"])
-	negative_text = pd.DataFrame(columns=["Text"])
 
-    	# Load Data
+	current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+	master_data = pd.DataFrame(columns=["Text"])
+
+	# Load Data
 	for file in os.listdir(r"../../data/processed"):
-		if file.endswith("TM_POS_DATA.csv"):
-			new_data = pd.read_csv(rf"../../data/processed/{file}").loc[:, ["Text"]]
-			positive_text = pd.concat([positive_text, new_data])
-		elif file.endswith("TM_NEG_DATA.csv"):
-			new_data = pd.read_csv(rf"../../data/processed/{file}").loc[:, ["Text"]]
-			negative_text = pd.concat([negative_text, new_data])
+		if file.endswith(".csv"):
+			new_data = pd.read_csv(rf"../../data/processed/{file}")
+			master_data = pd.concat([master_data, new_data])
+
+	## Separate into sentiments for topic modelling
+	positive_data = master_data.loc[master_data["Sentiment"] == "positive", ["Text"]]
+	negative_data = master_data.loc[master_data["Sentiment"] == "negative", ["Text"]]
 	
 	# Pipeline and Process data
 	min_len = 2
 	max_len = 15
-	positive_text["Text"] = positive_text["Text"].apply(lambda x: tm_preprocess(x, min_len, max_len))
-	negative_text["Text"] = negative_text["Text"].apply(lambda x: tm_preprocess(x, min_len, max_len))
+	positive_data["Text"] = positive_data["Text"].apply(lambda x: tm_preprocess(x, min_len, max_len))
+	negative_data["Text"] = negative_data["Text"].apply(lambda x: tm_preprocess(x, min_len, max_len))
 	# BOW
-	pos_dict = corpora.Dictionary(positive_text["Text"])
-	pos_corpus = [pos_dict.doc2bow(text) for text in positive_text["Text"]]
-	neg_dict = corpora.Dictionary(negative_text["Text"])
-	neg_corpus = [neg_dict.doc2bow(text) for text in negative_text["Text"]]
+	pos_dict = corpora.Dictionary(positive_data["Text"])
+	pos_corpus = [pos_dict.doc2bow(text) for text in positive_data["Text"]]
+	neg_dict = corpora.Dictionary(negative_data["Text"])
+	neg_corpus = [neg_dict.doc2bow(text) for text in negative_data["Text"]]
 	# TF_IDF
 	pos_tfidf_corpus = models.TfidfModel(pos_corpus)[pos_corpus]
 	neg_tfidf_corpus = models.TfidfModel(neg_corpus)[neg_corpus]
@@ -68,27 +71,68 @@ if __name__ == "__main__":
 	corpora.MmCorpus.serialize('pos_tfidf_corpus.mm', pos_tfidf_corpus)
 	corpora.MmCorpus.serialize('neg_tfidf_corpus.mm', neg_tfidf_corpus)
 	# Print the TF-IDF weights for each term in the document
-	"""
-	for i in range(len(pos_tfidf_corpus)):
-		pos_tfidf_weights = pos_tfidf_corpus[i]
-		for term_index, weight in pos_tfidf_weights:
-			print(f"Term '{pos_dict[term_index]}' has TF-IDF weight of {weight}")
-	"""
+	#for i in range(len(pos_tfidf_corpus)):
+	#	pos_tfidf_weights = pos_tfidf_corpus[i]
+	#	for term_index, weight in pos_tfidf_weights:
+	#		print(f"Term '{pos_dict[term_index]}' has TF-IDF weight of {weight}")
 
-	# Embedding for tfidf data
+	# Embedding for tfidf data using Word2Vec
+	# TODO: Separate out the parameters into configuration files
 	"""
 	size: Dimensionality of the embedding vectors
 	window: size of context window
 	min_count: minimum required count of the word for it to be included
-	workers: threads used during training
 	"""
-	pos_w2v = models.Word2Vec(sentences=pos_tfidf_corpus, size=100, window=5, min_count=5, workers=4)
-	neg_w2v = models.Word2Vec(sentences=neg_tfidf_corpus, size=100, window=5, min_count=5, workers=4)
+	pos_w2v = models.Word2Vec(sentences=pos_tfidf_corpus, vector_size=100, window=5, min_count=1)
+	neg_w2v = models.Word2Vec(sentences=neg_tfidf_corpus, vector_size=100, window=5, min_count=1)
+
+	# Store the whole model
+	#pos_w2v.save("pos_w2v.model") # pos_w2v = Word2Vec.load("pos_w2v.model")
+	#neg_w2v.save("neg_w2v.model") # neg_w2v = Word2Vec.load("neg_w2v.model")
+
+	# Store just the words + their trained embeddings.
+	pos_wv = pos_w2v.wv
+	pos_wv.save("pos_wv") #pos_wv = KeyedVectors.load("pos_wv", mmap='r')
+	neg_wv = neg_w2v.wv
+	neg_wv.save("neg_wv") #neg_wv = KeyedVectors.load("neg_wv", mmap='r')
+
+	# Dimension Reduction
+	pos_w2v_embeddings = []
+	for document in range(len(pos_tfidf_corpus)):
+		w2v_embedding = pos_w2v.wv[document]
+		pos_w2v_embeddings.append(w2v_embedding)
+
+	neg_w2v_embeddings = []
+	for document in range(len(neg_tfidf_corpus)):
+		w2v_embedding = neg_w2v.wv[document]
+		neg_w2v_embeddings.append(w2v_embedding)
+
+	n_components = 50
+	#svd = TruncatedSVD(n_components=n_components)
+	pca = PCA(n_components=n_components)
+	#svd_pos_embeddings = svd.fit_transform(pos_w2v_embeddings)
+	pca_pos_embeddings = pca.fit_transform(pos_w2v_embeddings)
+	pd.DataFrame(pca_pos_embeddings).to_csv("../data/tm/POS_PCA_EMBEDDING.csv", index = False, header = False)
+	#svd_neg_embeddings = svd.fit_transform(neg_w2v_embeddings)
+	pca_neg_embeddings = pca.fit_transform(neg_w2v_embeddings)
+	pd.DataFrame(pca_neg_embeddings).to_csv("../data/tm/NEG_PCA_EMBEDDING.csv", index = False, header = False)
 	
 
+	# PCA and SVD are both techniques for dimensionality reduction, which can be useful for topic modeling when dealing 
+	# with high-dimensional text data. By reducing the dimensionality of the data, it is possible to identify the most 
+	# important topics and extract more meaningful insights from the data.
+
+	# In terms of topic modeling, SVD is often preferred over PCA because SVD can provide a more accurate approximation 
+	# of the original data. SVD is also more computationally efficient than PCA for large datasets. However, PCA can be 
+	# useful when dealing with small datasets or when there is a need for interpretability of the extracted components.
 	
 
 
 
-            
-        
+
+	
+
+
+
+	    
+	
