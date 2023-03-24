536da72fff1c0dacd1ea8e8726be2f998e398817
Tsukumo1 <98806776+Tsukumo1@users.noreply.github.com>
2023-03-24 18:33:50 +0800

    Update feature_engineering_sa.py

--- src/preprocessing/feature_engineering_sa.py
+++ src/preprocessing/feature_engineering_sa.py
@@ -19,7 +19,19 @@ nltk.download('wordnet')
 nltk.download('stopwords')
 
 
+# remove the html symbol
+def remove_html(text):
+    regex = r"<[^>]+>"
+    text_new = re.sub(regex, " ", text)
+    return text_new
+
+
 def sa_preprocess(text):
+    # convert to lowercase
+    text = text.lower()
+    # remove non-alphabetic characters
+    text = re.sub(r'[^a-z]', ' ', text)
+    # tokenize the text into words
     tokens = word_tokenize(text)
     # remove stopwords
     stop_words = set(stopwords.words('english'))
@@ -39,49 +51,39 @@ def augment_text(text):
     return augmented_text
 
 
-# create a function to generate the word embedding vectors for each sentence
-def generate_word_embedding(sentence):
-    # initialize an empty array for the sentence vector
-    sentence_vector = []
-    # loop through each word in the sentence
-    for word in sentence:
-        try:
-            # add the vector representation of the word to the sentence vector
-            word_vector = word2vec_model.wv[word]
-            sentence_vector.append(word_vector)
-        except KeyError:
-            # ignore words that are not in the vocabulary
-            pass
-    # take the mean of the word vectors to get the sentence vector
-    sentence_vector = np.mean(sentence_vector, axis=0)
-    return sentence_vector
 
 
-
-def features_sa(train_data):
-    # apply the preprocessing function to the text data
-    train_data['Text'] = train_data['Text'].apply(preprocess_sa)
-
-    # apply the augmentation function to the preprocessed text data
-    train_data['Text'] = train_data['Text'].apply(augment_text)
-
+def over_sampling(train_data):
     # over sampling
     ros = RandomOverSampler(sampling_strategy='minority')
     X = train_data['Text'].values.reshape(-1, 1)
     y = train_data['Sentiment']
     X_resampled, y_resampled = ros.fit_resample(X, y)
     train_data = pd.DataFrame({'Text': X_resampled.ravel(), 'Sentiment': y_resampled})
+    return train_data
+
+
+
+
+def features_sa_train(train_data):
+    # apply the preprocessing function to the text data
+    train_data['Text'] = train_data['Text'].apply(remove_html)
+    train_data['Text'] = train_data['Text'].apply(sa_preprocess)
+
+    # apply the augmentation function to the preprocessed text data
+    train_data['Text'] = train_data['Text'].apply(augment_text)
+
+    # over sampling
+    train_data = over_sampling(train_data)
 
 
     ## Features
     # train a Word2Vec model on the preprocessed text data
     word2vec_model = Word2Vec(train_data['Text'], min_count=1)
-    # apply the generate_word_embedding() function to the preprocessed text data
-    train_data['embedding'] = train_data['Text'].apply(generate_word_embedding)
+    train_embeddings = train_data['Text'].apply(lambda x: np.mean([word2vec_model.wv[Text] for Text in x if Text in word2vec_model.wv.key_to_index], axis=0))
 
     # create a new DataFrame for the feature matrix
-    embedding_size = word2vec_model.vector_size
-    features_df = pd.DataFrame(train_data['embedding'].tolist(), columns=[f'embedding_{i}' for i in range(embedding_size)])
+    features_df = pd.DataFrame(train_embeddings.tolist(), index=train_embeddings.index)
 
     # perform PCA with n_components set to retain 98% of variance
     pca_emb = PCA(n_components=0.98)
@@ -93,13 +95,14 @@ def features_sa(train_data):
 
 
     # create a TF-IDF vectorizer object
-    tfidf_vectorizer = TfidfVectorizer()
+    tfidf = TfidfVectorizer()
 
-    # fit and transform the vectorizer on the preprocessed text data
-    tfidf_matrix = tfidf_vectorizer.fit_transform(train_data['Text'].apply(lambda x: ' '.join(x)))
+    # fit the vectorizer on the preprocessed text data
+    tfidf.fit(train_data['Text'].apply(lambda x: ' '.join(x)))
 
-    tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
-    tfidf_features_df = pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf_feature_names)
+    # obtain the TF-IDF feature matrix for the training and test data
+    train_matrix = tfidf.transform(train_data['Text'].apply(lambda x: ' '.join(x))).toarray()
+    tfidf_features_df = pd.DataFrame(train_matrix, columns=tfidf.get_feature_names_out())
     # perform PCA with n_components set to retain 95% of variance
     pca = PCA(n_components=0.95)
     features_tfidf_pca = pca.fit_transform(tfidf_features_df)
@@ -116,20 +119,58 @@ def features_sa(train_data):
     label = features_df.columns
     features_df['Sentiment'] = train_data['Sentiment']
 
-    # weight the negative sentiment samples by 1.5
+    # weight the negative sentiment samples by 2
     features_df.loc[features_df['Sentiment'] == 'negative',label] *= 2
 
-    return features_df
+    return features_df, word2vec_model, tfidf
 
-if __name__ == "__main__":
+def features_sa_test(test_data,word2vec_model, tfidf):
+    # apply the preprocessing function to the text data
+    test_data['Text'] = test_data['Text'].apply(remove_html)
+    test_data['Text'] = test_data['Text'].apply(sa_preprocess)
 
-	current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-	master_data = pd.DataFrame(columns=["Text"])
 
-	# Load Data
-	for file in os.listdir(r"../data/processed"):
-		if file.endswith(".csv"):
-			new_data = pd.read_csv(rf"../../data/processed/{file}")
-			master_data = pd.concat([master_data, new_data])
-                        
-    feature_matrix = features_sa(master_data)
+    # apply the augmentation function to the preprocessed text data
+    test_data['Text'] = test_data['Text'].apply(augment_text)
+
+    ## Features
+    # train a Word2Vec model on the preprocessed text data
+    test_embeddings = test_data['Text'].apply(lambda x: np.mean([word2vec_model.wv[Text] for Text in x if Text in word2vec_model.wv.key_to_index], axis=0))
+
+    # create a new DataFrame for the feature matrix
+    features_df = pd.DataFrame(test_embeddings.tolist(), index=test_embeddings.index)
+
+
+    # perform PCA with n_components set to retain 98% of variance
+    pca_emb = PCA(n_components=0.98)
+    features_emb_pca = pca_emb.fit_transform(features_df)
+
+    # create a new DataFrame for the PCA features
+    pca_emb_cols = [f"PC_emb{i+1}" for i in range(features_emb_pca.shape[1])]
+    pca_df_emb = pd.DataFrame(features_emb_pca, columns=pca_emb_cols)
+    
+
+    # obtain the TF-IDF feature matrix for the training and test data
+    test_matrix = tfidf.transform(test_data['Text'].apply(lambda x: ' '.join(x))).toarray()
+    tfidf_features_df = pd.DataFrame(test_matrix, columns=tfidf.get_feature_names_out())
+
+    # perform PCA with n_components set to retain 95% of variance
+    pca = PCA(n_components=0.95)
+    features_tfidf_pca = pca.fit_transform(tfidf_features_df)
+
+    # create a new DataFrame for the PCA features
+    pca_tfidf_cols = [f"PC_tfidf{i+1}" for i in range(features_tfidf_pca.shape[1])]
+    pca_df_tfidf = pd.DataFrame(features_tfidf_pca, columns=pca_tfidf_cols)
+
+    # add the TF-IDF features to the feature matrix DataFrame
+    features_df = pd.concat([pca_df_tfidf, pca_df_emb], axis=1)
+
+    # add the label column to the feature matrix DataFrame
+    label = features_df.columns
+    temp = pd.DataFrame(test_data['Sentiment'].tolist(),columns=["Sentiment"])
+    features_df = pd.concat([features_df, temp], axis=1)
+    
+    # weight the negative sentiment samples by 2
+    features_df.loc[features_df['Sentiment'] == 'negative',label] *= 2
+    return features_df,test_data["Sentiment"]
+
