3f32304b6454795a76f77d2e232291bd5d5f749a
Poto <enversionkiller@gmail.com>
2023-03-27 13:26:02 +0800

    Remove embedding and PCA part

--- src/preprocessing/tm_preprocessing.py
+++ src/preprocessing/tm_preprocessing.py
@@ -75,56 +75,6 @@ if __name__ == "__main__":
 	#	pos_tfidf_weights = pos_tfidf_corpus[i]
 	#	for term_index, weight in pos_tfidf_weights:
 	#		print(f"Term '{pos_dict[term_index]}' has TF-IDF weight of {weight}")
-
-	# Embedding for tfidf data using Word2Vec
-	# TODO: Separate out the parameters into configuration files
-	"""
-	size: Dimensionality of the embedding vectors
-	window: size of context window
-	min_count: minimum required count of the word for it to be included
-	"""
-	pos_w2v = models.Word2Vec(sentences=pos_tfidf_corpus, vector_size=100, window=5, min_count=1)
-	neg_w2v = models.Word2Vec(sentences=neg_tfidf_corpus, vector_size=100, window=5, min_count=1)
-
-	# Store the whole model
-	#pos_w2v.save("pos_w2v.model") # pos_w2v = Word2Vec.load("pos_w2v.model")
-	#neg_w2v.save("neg_w2v.model") # neg_w2v = Word2Vec.load("neg_w2v.model")
-
-	# Store just the words + their trained embeddings.
-	pos_wv = pos_w2v.wv
-	pos_wv.save("pos_wv") #pos_wv = KeyedVectors.load("pos_wv", mmap='r')
-	neg_wv = neg_w2v.wv
-	neg_wv.save("neg_wv") #neg_wv = KeyedVectors.load("neg_wv", mmap='r')
-
-	# Dimension Reduction
-	pos_w2v_embeddings = []
-	for document in range(len(pos_tfidf_corpus)):
-		w2v_embedding = pos_w2v.wv[document]
-		pos_w2v_embeddings.append(w2v_embedding)
-
-	neg_w2v_embeddings = []
-	for document in range(len(neg_tfidf_corpus)):
-		w2v_embedding = neg_w2v.wv[document]
-		neg_w2v_embeddings.append(w2v_embedding)
-
-	n_components = 50
-	#svd = TruncatedSVD(n_components=n_components)
-	pca = PCA(n_components=n_components)
-	#svd_pos_embeddings = svd.fit_transform(pos_w2v_embeddings)
-	pca_pos_embeddings = pca.fit_transform(pos_w2v_embeddings)
-	pd.DataFrame(pca_pos_embeddings).to_csv("../data/tm/POS_PCA_EMBEDDING.csv", index = False, header = False)
-	#svd_neg_embeddings = svd.fit_transform(neg_w2v_embeddings)
-	pca_neg_embeddings = pca.fit_transform(neg_w2v_embeddings)
-	pd.DataFrame(pca_neg_embeddings).to_csv("../data/tm/NEG_PCA_EMBEDDING.csv", index = False, header = False)
-	
-
-	# PCA and SVD are both techniques for dimensionality reduction, which can be useful for topic modeling when dealing 
-	# with high-dimensional text data. By reducing the dimensionality of the data, it is possible to identify the most 
-	# important topics and extract more meaningful insights from the data.
-
-	# In terms of topic modeling, SVD is often preferred over PCA because SVD can provide a more accurate approximation 
-	# of the original data. SVD is also more computationally efficient than PCA for large datasets. However, PCA can be 
-	# useful when dealing with small datasets or when there is a need for interpretability of the extracted components.
 	
 
 
