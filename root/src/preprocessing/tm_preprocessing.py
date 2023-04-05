d39a05c0ef56aefbbf03b2e30419a35cda6c1224
Poto <enversionkiller@gmail.com>
2023-04-05 21:00:51 +0800

    updated tm_preprocess to do all sentiments tgt

--- root/src/preprocessing/tm_preprocessing.py
+++ root/src/preprocessing/tm_preprocessing.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import pandas as pd
 import datetime
-from sklearn.decomposition import TruncatedSVD, PCA
 
 # Other NLP Libraries
 import nltk
@@ -25,54 +24,23 @@ def tm_preprocess_text(reviewText, min_len=2, max_len=15):
 	return lemmaText
 
 def TM_PREPROCESS_TRAIN(train_data, min_len=2, max_len=15):
-	positive_data = train_data.loc[train_data["Sentiment"] == "positive", ["Text"]]
-	negative_data = train_data.loc[train_data["Sentiment"] == "negative", ["Text"]]
-	positive_data["Text"] = positive_data["Text"].apply(lambda x: tm_preprocess_text(x, min_len, max_len))
-	negative_data["Text"] = negative_data["Text"].apply(lambda x: tm_preprocess_text(x, min_len, max_len))
+	train_data["Text"] = train_data["Text"].apply(lambda x: tm_preprocess_text(x, min_len, max_len))
 	
 	# BOW
 	# Create and Save the Dictionary of the Corpus
-	pos_dict = corpora.Dictionary(positive_data["Text"])
-	pos_corpus = [pos_dict.doc2bow(text) for text in positive_data["Text"]]
-	neg_dict = corpora.Dictionary(negative_data["Text"])
-	neg_corpus = [neg_dict.doc2bow(text) for text in negative_data["Text"]]
+	TM_BOW_dict = corpora.Dictionary(train_data["Text"])
+	TM_BOW_corpus = [pos_dict.doc2bow(text) for text in train_data["Text"]]
 
 	# TF_IDF
-	pos_tfidf = models.TfidfModel(pos_corpus)
-	pos_tfidf_corpus = pos_tfidf[pos_corpus]
-	neg_tfidf = models.TfidfModel(neg_corpus)
-	neg_tfidf_corpus = neg_tfidf[neg_corpus]
+	TM_tfidf = models.TfidfModel(TM_BOW_corpus)
+	TM_tfidf_corpus = TM_tfidf[TM_BOW_corpus]
 	
-	# TODO: Move this to Model_Train
-	#pos_tfidf.save('../../../models/tm/pos_tfidf')
-	#neg_tfidf.save('../../../models/tm/neg_tfidf')
-	# Saving the corpus and dict
-	"""
-	corpus: A list of document vectors, where each vector is represented as a 
-	tuple of word IDs and their corresponding frequencies.
-
-	dictionary: A mapping between words and their unique integer IDs in the corpus vocabulary.
-
-	tfidf_model: A term frequency-inverse document frequency (TF-IDF) model that assigns weights to 
-	words based on their frequency in the corpus and their relevance to the document.
-	"""
-	#pos_dict.save('../../../models/tm/pos_dict.dict') # can be loaded back using corpora.dictionary.load(fname)
-	#neg_dict.save('../../../models/tm/neg_dict.dict')
-	#corpora.MmCorpus.serialize('../../../models/tm/pos_corpus.mm', pos_corpus) # can be loaded back using corpora.MmCorpus(fname)
-	#corpora.MmCorpus.serialize('../../../models/tm/neg_corpus.mm', neg_corpus)
-	#corpora.MmCorpus.serialize('../../../models/tm/pos_tfidf_corpus.mm', pos_tfidf_corpus)
-	#corpora.MmCorpus.serialize('../../../models/tm/neg_tfidf_corpus.mm', neg_tfidf_corpus)
-	
-	return [pos_dict, neg_dict, pos_corpus, neg_corpus, pos_tfidf, neg_tfidf, pos_tfidf_corpus, neg_tfidf_corpus]
+	return [TM_BOW_dict, TM_BOW_corpus, TM_tfidf, TM_tfidf_corpus]
 
 def TM_PREPROCESS_TEST(test_data, min_len=2, max_len=15):
 	test_data["Sentiment"] = test_data["Sentiment"].apply(lambda x: x.lower().strip())
-	test_data["Sentiment"] = test_data["Sentiment"].apply(lambda x: x.lower().strip())
-	positive_data = test_data.loc[test_data["Sentiment"] == "positive"]
-	negative_data = test_data.loc[test_data["Sentiment"] == "negative"]
-	positive_data["processed_text"] = positive_data["Text"].apply(lambda x: tm_preprocess_text(x, min_len, max_len))
-	negative_data["processed_text"] = negative_data["Text"].apply(lambda x: tm_preprocess_text(x, min_len, max_len))
-	return positive_data, negative_data
+	test_data["processed_text"] = test_data["Text"].apply(lambda x: tm_preprocess_text(x, min_len, max_len))
+	return test_data
 
 if __name__ == "__main__":
 
@@ -86,7 +54,7 @@ if __name__ == "__main__":
 			new_data = pd.read_csv(rf"../../data/processed/{file}")
 			master_data = pd.concat([master_data, new_data])
 
-	pos_dict, neg_dict, pos_corpus, neg_corpus, pos_tfidf, neg_tfidf, pos_tfidf_corpus, neg_tfidf_corpus = TM_PREPROCESS_TRAIN(master_data)
+	TM_BOW_dict, TM_BOW_corpus, TM_tfidf, TM_tfidf_corpus = TM_PREPROCESS_TRAIN(master_data)
 
 	# Saving the corpus and dict
 	"""
@@ -99,16 +67,12 @@ if __name__ == "__main__":
 	words based on their frequency in the corpus and their relevance to the document.
 	"""
 	master_data.to_csv("../data/tm/{CURRENT_TIME}_CLEANED_DF.csv", index = False)
-	neg_tfidf.save('../../models/tm/neg_tfidf')
-	pos_tfidf.save('../../models/tm/pos_tfidf')
-	pos_dict.save('../../models/tm/pos_dict.dict') # can be loaded back using corpora.dictionary.load(fname)
-	neg_dict.save('../../models/tm/neg_dict.dict')
-	corpora.MmCorpus.serialize('../../models/tm/pos_corpus.mm', pos_corpus) # can be loaded back using corpora.MmCorpus(fname)
-	corpora.MmCorpus.serialize('../../models/tm/neg_corpus.mm', neg_corpus)
-	corpora.MmCorpus.serialize('../../models/tm/pos_tfidf_corpus.mm', pos_tfidf_corpus)
-	corpora.MmCorpus.serialize('../../models/tm/neg_tfidf_corpus.mm', neg_tfidf_corpus)
+	TM_tfidf.save('../../models/tm/tm_tfidf')
+	TM_BOW_dict.save('../../models/tm/tm_bow_dict.dict') # can be loaded back using corpora.dictionary.load(fname)
+	corpora.MmCorpus.serialize('../../models/tm/tm_bow_corpus.mm', TM_BOW_corpus) # can be loaded back using corpora.MmCorpus(fname)
+	corpora.MmCorpus.serialize('../../models/tm/tm_tfidf_corpus.mm', TM_tfidf_corpus)
 	# Print the TF-IDF weights for each term in the document
-	#for i in range(len(pos_tfidf_corpus)):
-	#	pos_tfidf_weights = pos_tfidf_corpus[i]
-	#	for term_index, weight in pos_tfidf_weights:
-	#		print(f"Term '{pos_dict[term_index]}' has TF-IDF weight of {weight}")
+	#for i in range(len(tm_tfidf_corpus)):
+	#	tm_tfidf_weights = tm_tfidf_corpus[i]
+	#	for term_index, weight in tm_tfidf_weights:
+	#		print(f"Term '{tm_bow_dict[term_index]}' has TF-IDF weight of {weight}")
